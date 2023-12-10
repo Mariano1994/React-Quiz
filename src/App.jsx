ec6a847dd3bbf002ffb03aa8f04c595ebde7fc72
Mariano <marianocapiliku@gmail.com>
2023-12-11 00:56:13 +0100

    Fetching Data to fake API using the resourse json-server Create state using useReducer Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import { Main } from "./components/Main";
 
+const initialState = {
+  questions: [],
+
+  //Possible Values of state: 'Loading', 'Error', 'Ready'. 'Active', 'Finished'
+  status: "Loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, action: action.payload, status: "ready" };
+    default:
+      return new Error("Action unknown");
+  }
+}
+
 export function App() {
+  const [state, dispacth] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((response) => response.json())
+      .then((data) => dispacth({ type: "dataReceived", payload: data }))
+      .catch((error) => console.error("Error"));
+  }, []);
+
   return (
     <>
       <div className="app">
